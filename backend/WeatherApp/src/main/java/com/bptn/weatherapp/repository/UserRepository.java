776025ba7297b4ffba86b3310a9f587ad115ec10
Hrithik Kumar Advani <hrithik.kumaradvani@mail.utoronto.ca>
2022-11-15 13:16:52 -0500

    Added UserRepository

---
+++ backend/WeatherApp/src/main/java/com/bptn/weatherapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bptn.weatherapp.repository;
+
+import com.bptn.weatherapp.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserById(int userId);
+}
